2de64658294897e86f867a827b2487f12a7b8445
cknwyn <tofferkent2005@gmail.com>
2026-04-20 19:26:17 +0800

    .

--- renderer.js
+++ renderer.js
@@ -94,15 +94,7 @@ async function initFirebase() {
             throw new Error("Firebase CDN scripts failed to load.");
         }
 
-        const firebaseConfig = {
-          apiKey: "YOUR_API_KEY",
-          authDomain: "YOUR_AUTH_DOMAIN",
-          projectId: "YOUR_PROJECT_ID",
-          storageBucket: "YOUR_STORAGE_BUCKET",
-          messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-          appId: "YOUR_APP_ID",
-          measurementId: "YOUR_MEASUREMENT_ID"
-        };
+        const firebaseConfig = require('./firebase-config.js');
 
         updateStatus("Connecting DB...");
         firebase.initializeApp(firebaseConfig);
